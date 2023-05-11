70ee09a9df2118b82bd3166e4fcaf6ee0c7c4103
Yash Raut 05 <131888738+yashraut05@users.noreply.github.com>
2023-05-11 20:46:16 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int  width;
+    int height;
+    int area;
+    int  perimeter;
+
+    height=7;
+    width=5;
+
+    cout << " Perimeter Of Reactangle= "<< 2*(height+width)<< " Inches" << endl;
+    cout << " Area Of Reactangle= "<< height*width << " Square Inches"<< endl;
+
+
+    return 0;
+}
